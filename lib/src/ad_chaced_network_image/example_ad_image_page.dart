f0849daa26490f93981930dae6078dc0e5f3a4b1
agus0211 <agus.suryanto@agriaku.com>
2022-12-13 14:05:58 +0700

    add new example

--- lib/src/ad_chaced_network_image/example_ad_image_page.dart
+++ lib/src/ad_chaced_network_image/example_ad_image_page.dart
@@ -15,7 +15,6 @@ class ExampleADImagePage extends StatefulWidget {
 
 class ExampleADImagePageState extends State<ExampleADImagePage> {
   late DateTime startTime;
-  // https://picsum.photos/500
   final List<ItemParam> items = [
     ItemParam(
         title: 'JPEG Image',
@@ -25,6 +24,54 @@ class ExampleADImagePageState extends State<ExampleADImagePage> {
         title: 'PNG Image',
         url:
             'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2Ffirebase-logo.png?alt=media&token=966dfd64-75c0-4693-83a0-f9f5e7068fc4'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2FScreenshot_20220920-180431_Staging%20-%20AgriAku%20Mitra.jpg?alt=media&token=49cb8648-073f-4144-b483-a33f9cd2d960'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2FScreenshot_20211122-110032.jpg?alt=media&token=7f386eef-fb22-4112-8f2b-70e41958f9ba'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2F2022-09-26%2014_29_21-Enhance%20Tracking%20-%20Google%20Sheets.png?alt=media&token=a83faa23-5fec-483c-96b4-9a0957d13917'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2F2022-09-23%2001_50_21-2022-09-23%2001_49_50-2000.jpg%20(3000%C3%972000).png%20%E2%80%8E-%20Photos.png?alt=media&token=19db2b8a-c2d2-4291-af2a-2a10be4ab8d9'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2F2022-07-25%2013_55_07-NEW%20Mitra%20App%20%E2%80%93%20Figma.png?alt=media&token=bc674511-82b1-45ec-b889-abe9fbd58025'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2F2022-08-25%2016_11_04-.png?alt=media&token=11642838-9a08-4549-b563-f3fb2bff0d9b'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2F2022-07-27%2013_39_33-Backend%20Master%20Docs%20-%20Google%20Docs.png?alt=media&token=cd40d93f-1809-448d-9bbb-67a1dad79693'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2Forder_confirmation-_summary.png?alt=media&token=137f8aee-cad2-4e16-8ad8-81d476088e6b'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2FCari%20Barang.png?alt=media&token=968d06ec-7c93-4816-9767-8d72777c87ec'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2Fempty_purchased.png?alt=media&token=e4907aa5-60af-4087-a462-18dfbcc8c61e'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2Fexample_image.png?alt=media&token=59d38b98-ded2-4d27-8159-772669e3a47e'),
+    ItemParam(
+        title: 'PNG Image',
+        url:
+            'https://firebasestorage.googleapis.com/v0/b/ecotrade-81289.appspot.com/o/testing_images%2Fss.png?alt=media&token=1ec2d2ba-6626-4ef8-a47f-61aa39a1651a'),
     ItemParam(title: 'Random Image', url: 'https://picsum.photos/500'),
     ItemParam(title: 'Random Image', url: 'https://picsum.photos/600'),
     ItemParam(title: 'Random Image', url: 'https://picsum.photos/1024'),
@@ -78,7 +125,7 @@ class ExampleADImagePageState extends State<ExampleADImagePage> {
       final diff = endTime.difference(startTime).inMilliseconds;
       String start = DateFormat('HH:mm:ss SSSS').format(startTime);
       String end = DateFormat('HH:mm:ss SSSS').format(endTime);
-      print('FCP: $start - $end : $diff ms');
+      debugPrint('FCP: $start - $end : $diff ms');
     });
   }
 
